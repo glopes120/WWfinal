9b23b3111983fb71ed1494c2b09603f8498a22b4
glopes120 <145976765+glopes120@users.noreply.github.com>
2026-01-14 15:43:58 +0000

    Add files via upload

--- wisewallet-app/wisewallet-app/vite.config.js
+++ wisewallet-app/wisewallet-app/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Isto diz ao Vite: "Se vires um pedido para /api, envia para o backend na porta 3004"
     proxy: {
-      '/gemini-parse': {
-        target: 'http://localhost:3003', // Your Express server's address
-      },
-    },
-  },
-});
+      '/api': {
+        target: 'http://localhost:3004',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
